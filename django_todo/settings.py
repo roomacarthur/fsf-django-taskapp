02ab4888b2ed474ae2a6ad24de251360a211044a
roomacarthur <roomacarthur@gmail.com>
2021-07-27 15:58:47 +0100

    updated config

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -16,8 +16,6 @@ import os
 from decouple import config
 
 
-development = os.environ.get("DEVELOPMENT", True)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", config("SECRET_KEY"))
+SECRET_KEY = ["SECRET_KEY", config("SECRET_KEY")]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = config("DEBUG", cast=bool, default="False")
 
-ALLOWED_HOSTS = ["127.0.0.1", os.environ.get("HEROKU_HOSTNAME")]
+ALLOWED_HOSTS = ["127.0.0.1", "HEROKU_HOSTNAME"]
 
 # if development:
 #     ALLOWED_HOSTS = ["127.0.0.1"]
