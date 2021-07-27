8715314e3a980e11cf323ee6c06ec73cb0a57241
roomacarthur <roomacarthur@gmail.com>
2021-07-27 16:07:18 +0100

    allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = ["SECRET_KEY", config("SECRET_KEY")]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool, default="False")
 
-ALLOWED_HOSTS = ["127.0.0.1", "HEROKU_HOSTNAME"]
+ALLOWED_HOSTS = [os.environ.get("HEROKU_HOSTNAME"), "127.0.0.1"]
 
 # if development:
 #     ALLOWED_HOSTS = ["127.0.0.1"]
